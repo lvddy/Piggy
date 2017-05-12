e2d75945aeabe86f3519f8b8c5fec6a8ebbb948b
lvdi <lvdi@kuaiyd.com>
2017-05-12 16:39:44 +0800

    add discovery

--- piggy-account/src/main/java/com/lvddy/piggy/account/AccountApplication.java
+++ piggy-account/src/main/java/com/lvddy/piggy/account/AccountApplication.java
@@ -2,11 +2,13 @@ package com.lvddy.piggy.account;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * Created by Administrator on 2017/5/12 0012.
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class AccountApplication {
 
     public static void main(String[] args) {
--- piggy-config/src/main/java/com/lvddy/piggy/config/ConfigServerApplication.java
+++ piggy-config/src/main/java/com/lvddy/piggy/config/ConfigServerApplication.java
@@ -3,16 +3,20 @@ package com.lvddy.piggy.config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.config.server.environment.EnvironmentRepository;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * Created by Administrator on 2017/5/11 0011.
  */
 @SpringBootApplication
 @EnableConfigServer
+@EnableEurekaClient
 public class ConfigServerApplication {
 
 
     public static void main(String[] args) {
         SpringApplication.run(ConfigServerApplication.class,args);
+       // EnvironmentRepository
     }
 }
