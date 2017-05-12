ace67d8ad4b3aa6324ea373a48b4a91713195611
lvdi <lvdi@kuaiyd.com>
2017-05-12 18:00:01 +0800

    add monitor to piggy

--- piggy-monitor/src/main/java/MonitorApplication.java
+++ piggy-monitor/src/main/java/MonitorApplication.java
@@ -1,9 +1,15 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
 /**
  * Created by kcz-020 on 2017/5/12.
  */
+@SpringBootApplication
+@EnableEurekaClient
 public class MonitorApplication {
 
     public static void main(String[] args) {
-
+        SpringApplication.run(MonitorApplication.class, args);
     }
 }
