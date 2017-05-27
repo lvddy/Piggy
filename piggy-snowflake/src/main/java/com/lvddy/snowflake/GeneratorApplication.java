d03d91bc7b1791adf1bb41101bc93d31050385c3
lvdi <lvdi@kuaiyd.com>
2017-05-27 14:35:30 +0800

    Merge branch 'master' of dev.igrowiser.com:liaohongjian/weike
    # Conflicts:
    #	weike-program/src/test/java/net/weike/test/controller/CourseControllerTest.java
    #	weike-service/src/main/java/net/weike/service/impl/CourseServiceImpl.java

--- piggy-snowflake/src/main/java/com/lvddy/snowflake/GeneratorApplication.java
+++ piggy-snowflake/src/main/java/com/lvddy/snowflake/GeneratorApplication.java
@@ -2,13 +2,12 @@ package com.lvddy.snowflake;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * Created by kcz-020 on 2017/5/23.
  */
 @SpringBootApplication
-@EnableEurekaClient
+//@EnableEurekaClient
 public class GeneratorApplication {
 
 
--- piggy-snowflake/src/main/java/com/lvddy/snowflake/config/ZookeeperConfig.java
+++ piggy-snowflake/src/main/java/com/lvddy/snowflake/config/ZookeeperConfig.java
@@ -1,6 +1,6 @@
 package com.lvddy.snowflake.config;
 
-import org.apache.zookeeper.WatchedEvent;
+import lombok.extern.log4j.Log4j2;
 import org.apache.zookeeper.ZooKeeper;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -10,18 +10,20 @@ import org.springframework.context.annotation.Configuration;
  * Created by kcz-020 on 2017/5/24.
  */
 @Configuration
+@Log4j2
 public class ZookeeperConfig {
 
-    @Value("zookeeper.connection")
+    @Value("${zookeeper.connection}")
     private String connection;
 
-    @Value("zookeeper.timeout")
-    private int timeout;
+    @Value("${zookeeper.timeout}")
+    private String timeout;
 
     @Bean
     public ZooKeeper zooKeeper() throws Exception{
-        return new ZooKeeper(connection, timeout, (event)->{
-                System.out.println("已经触发了" + event.getType() + "事件！");
+        System.out.println(timeout);
+        return new ZooKeeper(connection, Integer.valueOf(timeout), (event)->{
+                log.info("连接zookeeper成功");
         });
     }
 
